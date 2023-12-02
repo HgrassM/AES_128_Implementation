fa26ab3810cd31ec6d2985f08d910bb2be2a889b
HgrassM <119745077+HgrassM@users.noreply.github.com>
2023-12-02 20:14:08 -0300

    Teste das funções
    A função main possui um código para testar as funções do módulo key_expansion

--- main.rs
+++ main.rs
@@ -1,3 +1,32 @@
+mod key_expansion;
+
 fn main() {
-    //TODO: Implement
+    //Testando as funções
+    
+    let string: String = String::from("D5fg");
+    let mut array:[u8;4] = [0;4];
+    let mut x = 0;
+    for b in string.bytes() {
+        array[x] = b;
+        x += 1;
+    }
+
+    let bytes: [u8;4] = key_expansion::rot_word(array);
+    let mut vector: Vec<u8> = Vec::new();
+    vector.push(bytes[0]);
+    vector.push(bytes[1]);
+    vector.push(bytes[2]);
+    vector.push(bytes[3]);
+    let s = String::from_utf8_lossy(&vector);
+    println!("{s} rot word");
+
+    let bytes2 = key_expansion::sub_word(array);
+    let mut vector2: Vec<u8> = Vec::new();
+    vector2.push(bytes2[0]);
+    vector2.push(bytes2[1]);
+    vector2.push(bytes2[2]);
+    vector2.push(bytes2[3]);
+    let s2 = String::from_utf8_lossy(&vector2);
+    println!("{s2} sub word");
+
 }
