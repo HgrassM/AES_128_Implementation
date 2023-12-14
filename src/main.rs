f373a1aeeca1990fe74bcdf65170976531e94198
Bernardo Rabello <53843870+BernardoRabello@users.noreply.github.com>
2023-12-14 04:40:02 -0300

    "merging" branch do Luiz

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,46 @@ mod funcoes;
 use std::fs;
 use std::env;
 
+use eframe::{egui, run_native, NativeOptions, App, CreationContext};
+
+
+struct MyApp {
+    input_path: String,
+    output_path: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            input_path: String::new(),
+            output_path: String::new(),
+        }
+    }
+}
+
+impl App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Encriptação");
+            ui.horizontal(|ui| {
+                ui.label("Path:");
+                ui.text_edit_singleline(&mut self.input_path);
+            });
+            if ui.button("Enviar").clicked() {
+                self.output_path = self.input_path.clone();
+            }
+            if !self.output_path.is_empty() {
+                ui.label(format!("Caminho Inserido: {}", self.output_path));
+            }
+        });
+    }
+}
 
 fn main() {
-    
-    let method = env::args().nth(1).expect("Missing arguments\nUse -h to get help").to_lowercase();
-    if method == "-h" {
-        
-        println!(
-"Usage: .\\aes128.exe ... [option] ... [arg]
-Options:
--e    : Encrypts a file, a file path is required, if no key is specified a random one will be generated
--d    : Decode an encrypted file, a file path and a key is required
--h    : Show this page
-Arguments:
-file  : Path to a specific file for encrypting or decrypting
-key   : Numeric key used for decrypting, optional for encrypting, must not be longer than 128 bits"
-        );
-
-    } else if method == "-e" {
-        //encrypt
-        let dir = env::args().nth(2).expect("Missing arguments\nUse -h to get help");
-        let file = fs::read(dir).expect("No file found");
-        
-        let key = (0x5468617473206D79204B756E67204675 as u128).to_be_bytes();
-
-        let codificado = aes128::encrypt(file, key);
-        println!("{:0x?}", codificado);
-        
-
-
-    } else if method == "-d" {
-        //decrypt
-        let dir = env::args().nth(2).expect("Missing arguments\nUse -h to get help");
-        let file = fs::read(dir).expect("No file found");
-        
-        let key = (0x5468617473206D79204B756E67204675 as u128).to_be_bytes();
-
-        println!("{:0x?}", aes128::decrypt(file, key));
-
-    } else {
-        println!("Unkown argument: {}\nUse -h to get help", method);
-    }
+    let options = NativeOptions::default();
+    run_native(
+        "TRABALHO LP",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
 }
